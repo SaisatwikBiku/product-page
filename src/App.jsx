05a389914b627eb8d7300f28fbf07d74bc066f59
SaisatwikBiku <bsaisatwik@yahoo.com>
2025-06-30 15:33:46 -0400

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ function App() {
 
         <ul>
           <li href="#">Menu</li>
-          <li href="#">Menu</li>
-          <li href="#">Menu</li>
-          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
         </ul>
+
+        <button>Login</button>
       </nav>
     </div>
   )
